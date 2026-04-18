b846a8efbaed677bac10ba3accaefa51c9873111
macsnoeren <macsnoeren@gmail.com>
2026-04-18 15:44:06 +0200

    Last changes

--- data/output/todos/start_todo_lijst.bat
+++ data/output/todos/start_todo_lijst.bat
@@ -5,7 +5,7 @@ setlocal
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set datetime=%%I
 set current_date=%datetime:~0,8%
 
-set target_dir=..\..\..\..\data\input\todos
+set target_dir=..\..\..\data\input\todos
 set target_file=%target_dir%\%current_date%-todo.txt
 
 :: Maak de directory aan als deze nog niet bestaat
